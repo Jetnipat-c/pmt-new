f708859f9d6b4932fd436b86917bc087cc01ea93
alonecheer <alonecheer@hotmail.com>
2021-03-09 02:56:37 +0700

    :zap: ok

--- layouts/Layouts.tsx
+++ layouts/Layouts.tsx
@@ -5,10 +5,62 @@ const Layouts = (props) => {
     <React.Fragment>
       <StyleWrapper>
         <div className="container">
-          <div className="header">header</div>
-          <div className="main">main{props.children}</div>
-          <div className="sidebar">sidebar</div>
-          <div className="footer">footer</div>
+          <div className="header">
+            <div className="sub-header-grid">
+              <div className="sub-items">
+                <div className="logo">
+                  <i className="fa fa-glass logo" aria-hidden="true"></i>
+                  Patthotech
+                </div>
+              </div>
+              <div className="sub-item">
+                <div className="user">username</div>
+              </div>
+            </div>
+          </div>
+          <div className="main">{props.children}</div>
+          <div className="sidebar">
+            <div className="menu-sidebar-grid">
+              <div className="grid-item">
+                <i className="fa fa-home logo" aria-hidden="true"></i>หน้าแรก
+              </div>
+              <div className="grid-item">
+                <div className="dropdown-grid">
+                  <i className="fa fa-cog logo" aria-hidden="true"></i>
+                  จัดการสินค้า
+                </div>
+                <div className="menu-dropdown">
+                  <a href="#">
+                    <i className="fa fa-plus logo" aria-hidden="true"></i>
+                    เพิ่มสินค้า
+                  </a>
+                </div>
+                <div className="menu-dropdown">
+                  <a href="#">
+                    <i className="fa fa-search logo" aria-hidden="true"></i>
+                    ค้นหาสินค้า
+                  </a>
+                </div>
+              </div>
+              <div className="grid-item">
+                <i className="fa fa-pencil logo" aria-hidden="true"></i>
+                บันทึกขั้นตอนการทำงาน
+              </div>
+              <div className="grid-item">
+                <i className="fa fa-tags logo" aria-hidden="true"></i>
+                ตรวจเช็คกระจก
+              </div>
+              <div className="grid-item">
+                <i className="fa fa-sign-out logo" aria-hidden="true"></i>
+                ออกจากระบบ
+              </div>
+            </div>
+          </div>
+          <div className="footer">
+            <div className="text">
+              Pattothec Design ©2021 Created by Pattothec Company
+            </div>
+          </div>
         </div>
       </StyleWrapper>
     </React.Fragment>
--- styles/components/layouts/styles.tsx
+++ styles/components/layouts/styles.tsx
@@ -5,46 +5,97 @@ const StyleWrapper = styled.div`
     grid-template:
       "header header" 1fr
       "sidebar main" 90vh
-      "footer footer" 1fr
+      "footer footer" 40px
       / 300px 3fr;
     grid-gap: 10px;
-    color: #fff;
-    font-weight: bold;
+    color: var(--light_gray);
     text-align: center;
+    font-size: 1.3rem;
   }
   .container > * {
     padding: 10px;
   }
   .header {
     background: var(--light_gray);
-    grid-area: header;
+    //grid-area: header;
+    color: var(--white);
+    text-align: center;
+    font-size: 2rem;
   }
   .main {
     background: var(--white);
     grid-area: main;
     //border-top: 1px solid var(--light_purple);
+    color: #9e9e9e;
   }
   .sidebar {
     background: var(--purple);
     grid-area: sidebar;
+    text-align: left;
+  }
+  .sidebar .grid-item i {
+    margin-left: 1rem;
+  }
+  .menu-sidebar-grid .grid-item .menu-dropdown a {
+    margin-left: 2rem;
   }
   .footer {
     background: var(--light_purple);
     grid-area: footer;
+    color: #fff;
+    font-size: 1rem;
+  }
+  .logo {
+    margin-right: 1rem;
+  }
+  .sub-item .user {
+    font-size: 1rem;
+  }
+  // SideBar
+  .menu-sidebar-grid {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 10px 0;
+  }
+  // Dropdown
+  .menu-dropdown {
+    display: none;
+    margin: 20px 0;
+  }
+  .grid-item:hover .menu-dropdown {
+    display: grid;
+  }
+  .grid-item a {
+    color: var(--light_gray);
+  }
+  .grid-item a:hover {
+    color: var(--white);
+  }
+  .grid-item:hover {
+    color: var(--white);
+    cursor: pointer;
   }
   @media only screen and (max-width: 425px) {
     .container {
       display: grid;
       grid-template:
         "header" 1fr
-        "sidebar" 300px
+        "sidebar" auto
         "main" 90vh
         "footer" 1fr
         / 1fr;
       //grid-gap: 10px;
       color: #fff;
-      font-weight: bold;
       text-align: center;
+    }
+    .sidebar {
+      font-size: 1rem;
+    }
+    .header {
+      font-size: 2rem;
+    }
+    .footer {
+      font-size: 1.3rem;
     }
   }
 `;
